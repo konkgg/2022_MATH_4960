9088f91799505faaa022413ff961c15b65d87c78
Konrad <98121791+ksturdivantwilson@users.noreply.github.com>
2022-09-03 22:24:10 -0400

    Reworked TestPermutations

--- TestPermutations.java
+++ TestPermutations.java
@@ -3,33 +3,34 @@ import java.util.ArrayList;
 public class TestPermutations {
     public static void main(String[] args)
     {
-        System.out.println("input array test");
-        int[] testArray = {0, 1, 2, 3, 4, 5,};
+        // input array test
+        int[] testArray = {0, 1, 2, 3, 4, 5};
         Permutations testArrayInput = new Permutations(testArray);
-        for(int i = 0; i < testArrayInput.numbers.length; i++)
-        {
-            System.out.println(testArrayInput.numbers[i] + 1);
-        }
-
-        System.out.println("input arraylist test");
+        // ArrayList Test
         ArrayList<Integer> tali = new ArrayList<>();
         for(int i = 0; i < 6; i++)
         {
             tali.add(i);
         }
-
         Permutations testArrayListInput = new Permutations(tali);
-        for(int i = 0; i < testArrayListInput.numbers.length; i++)
-        {
-            System.out.println(testArrayListInput.numbers[i] + 1);
-        }
 
-
-        System.out.println("Initiator test");
+        // Initiator Test
         Permutations testIntInitiator = new Permutations(6);
-        for(int i = 0; i < testIntInitiator.numbers.length; i++)
+
+        for(int i = 0; i < 6; i++)
         {
-            System.out.println(testIntInitiator.numbers[i] + 1);
+            if(i < testArrayInput.numbers.length)
+            {
+                System.out.println("ArrayInput : " + testArrayInput.numbers[i]);
+            }
+            if(i < testArrayListInput.numbers.length)
+            {
+                System.out.println("ArrayList : " + testArrayListInput.numbers[i]);
+            }
+            if(i < testIntInitiator.numbers.length)
+            {
+                System.out.println(String.format("Initiator : %d", testIntInitiator.numbers[i] + 1));
+            }
         }
     }
 }
