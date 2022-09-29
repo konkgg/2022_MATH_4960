82eddf630cf602e8635a8cfc5ec412884861ca49
Konrad <98121791+ksturdivantwilson@users.noreply.github.com>
2022-09-29 11:46:46 -0400

    Add files via upload
    Reintroduced display method
    Fixed some issues with file reading

--- Configuration.java
+++ Configuration.java
@@ -99,28 +99,61 @@ public class Configuration {
         this.permutations = permutations;
     }
 
+
     //Display Method
-    public String DisplayMethod()
+        public String DisplayMethod()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Permutation\t");
+            for(int i = 0; i < dieSides; i++)
+            {
+                sb.append(String.format("P%d\t",i + 1));
+            }
+    
+            for(int i = 0; i < permutations.length; i++)
+            {
+                sb.append(String.format("%n Die %d: ",(i + 1)));
+                for(int k = 0; k < permutations[i].numbers.length; k++)
+                {
+                    sb.append("\t");
+                    sb.append(permutations[i].numbers[k] + 1);
+                }
+                sb.append("\t");
+                sb.append(String.format("RowSum: %d", getRowSum(i)));
+    
+            }
+            sb.append("\n ColumnSum");
+            for(int i = 0; i < dieSides; i++)
+            {
+                sb.append(String.format("\t%d",getColumn(i)));
+            }
+            return sb.toString();
+        }
+    /* Math methods
+    had to add one to each number due to permutation making pos 0 equal 0 instead of 1 and us just adding one in the display
+    */
+    public int getRowSum(int a)
     {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Permutation\t");
+        int sum = 0;
         for(int i = 0; i < dieSides; i++)
         {
-            sb.append(String.format("P%d\t",i + 1));
+            sum += permutations[a].numbers[i] + 1;
         }
-
-        for(int i = 0; i < permutations.length; i++)
-        {
-            sb.append(String.format("%n Die %d: ",(i + 1)));
-            for(int k = 0; k < permutations[i].numbers.length; k++)
-            {
-                sb.append("\t");
-                sb.append(permutations[i].numbers[k]);
-            }
-
-        }
-        return sb.toString();
+        return sum;
     }
+
+    public int getColumn(int a)
+    {
+        int sum = 0;
+        for(int i = 0; i < dieCount; i++)
+        {
+            sum += permutations[i].numbers[a] + 1;
+        }
+
+        return sum;
+    }
+
+
 
     //toString
     @Override
